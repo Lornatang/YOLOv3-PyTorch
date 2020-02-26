b1ea14b998178a87f11ac30ed662ed6f6e76a352
changyu98 <liuchangyu98@outlook.com>
2020-02-26 20:47:30 +0800

    Formatted output code style modification

--- script/create_voc_datasets.py
+++ script/create_voc_datasets.py
@@ -12,11 +12,11 @@
 # limitations under the License.
 # ==============================================================================
 
+# Source: https://github.com/pjreddie/darknet/blob/master/scripts/voc_label.py
 import os
 import xml.etree.ElementTree
-from os import getcwd
 
-sets = [('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets = [("2007", "train"), ("2007", "val"), ("2007", "test")]
 
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog",
            "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
@@ -37,38 +37,35 @@ def convert(size, box):
 
 
 def convert_annotation(year, image_id):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml' % (year, image_id))
-    out_file = open('VOCdevkit/VOC%s/labels/%s.txt' % (year, image_id), 'w')
+    in_file = open(f"VOCdevkit/VOC{year}/Annotations/{image_id}.xml")
+    out_file = open(f"VOCdevkit/VOC{year}/labels/{image_id}.txt", "w")
     tree = xml.etree.ElementTree.parse(in_file)
     root = tree.getroot()
-    size = root.find('size')
-    w = int(size.find('width').text)
-    h = int(size.find('height').text)
+    size = root.find("size")
+    w = int(size.find("width").text)
+    h = int(size.find("height").text)
 
-    for obj in root.iter('object'):
-        difficult = obj.find('difficult').text
-        cls = obj.find('name').text
+    for obj in root.iter("object"):
+        difficult = obj.find("difficult").text
+        cls = obj.find("name").text
         if cls not in classes or int(difficult) == 1:
             continue
         cls_id = classes.index(cls)
-        xmlbox = obj.find('bndbox')
-        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
-             float(xmlbox.find('ymax').text))
+        xmlbox = obj.find("bndbox")
+        b = (float(xmlbox.find("xmin").text), float(xmlbox.find("xmax").text), float(xmlbox.find("ymin").text),
+             float(xmlbox.find("ymax").text))
         bb = convert((w, h), b)
-        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + "\n")
 
 
-wd = getcwd()
+current_path = os.getcwd()
 
-for year, image_set in sets:
-    if not os.path.exists('VOCdevkit/VOC%s/labels/' % year):
-        os.makedirs('VOCdevkit/VOC%s/labels/' % year)
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt' % (year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt' % (year, image_set), 'w')
-    for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n' % (wd, year, image_id))
-        convert_annotation(year, image_id)
+for years, image_set in sets:
+    if not os.path.exists(f"VOCdevkit/VOC{years}/labels/"):
+        os.makedirs(f"VOCdevkit/VOC{years}/labels/")
+    image_indexs = open(f"VOCdevkit/VOC{years}/ImageSets/Main/{image_set}.txt").read().strip().split()
+    list_file = open(f"{years}_{image_set}.txt", "w")
+    for image_index in image_indexs:
+        list_file.write(f"{current_path}/VOCdevkit/VOC{years}/JPEGImages/{image_index}.jpg\n")
+        convert_annotation(years, image_index)
     list_file.close()
-
-os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train.txt")
-os.system("cat 2007_train.txt 2007_val.txt 2007_test.txt 2012_train.txt 2012_val.txt > train.all.txt")
