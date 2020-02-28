c72c8fc0b0d76ec1f9008a121b3650df70849e0c
changyu98 <liuchangyu98@outlook.com>
2020-02-28 16:28:00 +0800

    Version information has been added to each file

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+# Copyright 2020 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import argparse
 import json
 
@@ -21,7 +34,7 @@ def test(cfg,
          dataloader=None):
     # Initialize/load model and set device
     if model is None:
-        device = utils.select_device(opt.device, batch_size=batch_size)
+        device = torch_utils.select_device(opt.device, batch_size=batch_size)
         verbose = opt.task == 'test'
 
         # Remove previous
@@ -32,7 +45,6 @@ def test(cfg,
         model = Darknet(cfg, img_size).to(device)
 
         # Load weights
-        attempt_download(weights)
         if weights.endswith('.pth'):  # pytorch format
             model.load_state_dict(torch.load(weights, map_location=device)['model'])
         else:  # darknet format
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,16 @@
+# Copyright 2020 Lorna Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import glob
 import math
 import os
@@ -16,7 +29,7 @@ from tqdm import tqdm
 
 from utils.utils import xyxy2xywh, xywh2xyxy
 
-help_url = 'https://github.com/ultralytics/yolov3/wiki/Train-Custom-Data'
+help_url = 'https://github.com/Lornatang/YOLOv3-PyTorch#train-on-custom-dataset'
 img_formats = ['.bmp', '.jpg', '.jpeg', '.png', '.tif', '.dng']
 vid_formats = ['.mov', '.avi', '.mp4']
 
