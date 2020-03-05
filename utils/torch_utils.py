f0572bc3b404820550d564216ecf1ddcf77a1099
lornatang <liuchangyu98@outlook.com>
2020-03-05 12:52:34 +0800

    change device select mothod

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -27,31 +27,30 @@ def init_seeds(seed=0):
         cudnn.benchmark = False
 
 
-def select_device(device='', apex=False, batch_size=None):
-    # device = 'cpu' or '0' or '0,1,2,3'
-    cpu_request = device.lower() == 'cpu'
-    if device and not cpu_request:  # if device requested other than 'cpu'
-        os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
-        assert torch.cuda.is_available(), 'CUDA unavailable, invalid device %s requested' % device  # check availablity
+def select_device(device="", apex=False, batch_size=None):
+    # device = "cpu" or "cuda:0"
+    cpu_request = device.lower() == "cpu"
+    if device and not cpu_request:  # if device requested other than "cpu"
+        os.environ["CUDA_VISIBLE_DEVICES"] = device  # set environment variable
+        assert torch.cuda.is_available(), f"CUDA unavailable, invalid device {device} requested"
 
     cuda = False if cpu_request else torch.cuda.is_available()
     if cuda:
         c = 1024 ** 2  # bytes to MB
-        ng = torch.cuda.device_count()
-        if ng > 1 and batch_size:  # check that batch_size is compatible with device_count
-            assert batch_size % ng == 0, 'batch-size %g not multiple of GPU count %g' % (batch_size, ng)
-        x = [torch.cuda.get_device_properties(i) for i in range(ng)]
-        s = 'Using CUDA ' + ('Apex ' if apex else '')  # apex for mixed precision https://github.com/NVIDIA/apex
-        for i in range(0, ng):
+        gpu_count = torch.cuda.device_count()
+        if gpu_count > 1 and batch_size:  # check that batch_size is compatible with device_count
+            assert batch_size % gpu_count == 0, f"batch-size {batch_size} not multiple of GPU count {gpu_count}"
+        x = [torch.cuda.get_device_properties(i) for i in range(gpu_count)]
+        s = "Using CUDA " + ("Apex " if apex else "cpu")
+        for i in range(0, gpu_count):
             if i == 1:
-                s = ' ' * len(s)
-            print("%sdevice%g _CudaDeviceProperties(name='%s', total_memory=%dMB)" %
-                  (s, i, x[i].name, x[i].total_memory / c))
+                s = " " * len(s)
+            print(f"{s}\n\t* device:{i} (name=`{x[i].name}`, total_memory={int(x[i].total_memory / c)}MB)")
     else:
-        print('Using CPU')
+        print("Using CPU")
 
-    print('')  # skip a line
-    return torch.device('cuda:0' if cuda else 'cpu')
+    print("")  # skip a line
+    return torch.device("cuda:0" if cuda else "cpu")
 
 
 def fuse_conv_and_bn(conv, bn):
@@ -81,24 +80,24 @@ def fuse_conv_and_bn(conv, bn):
         return fusedconv
 
 
-def model_info(model, report='summary'):
+def model_info(model, report="summary"):
     # Plots a line-by-line description of a PyTorch model
     n_p = sum(x.numel() for x in model.parameters())  # number parameters
     n_g = sum(x.numel() for x in model.parameters() if x.requires_grad)  # number gradients
-    if report is 'full':
-        print('%5s %40s %9s %12s %20s %10s %10s' % ('layer', 'name', 'gradient', 'parameters', 'shape', 'mu', 'sigma'))
+    if report is "full":
+        print("%5s %40s %9s %12s %20s %10s %10s" % ("layer", "name", "gradient", "parameters", "shape", "mu", "sigma"))
         for i, (name, p) in enumerate(model.named_parameters()):
-            name = name.replace('module_list.', '')
-            print('%5g %40s %9s %12g %20s %10.3g %10.3g' %
+            name = name.replace("module_list.", "")
+            print("%5g %40s %9s %12g %20s %10.3g %10.3g" %
                   (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
-    print('Model Summary: %g layers, %g parameters, %g gradients' % (len(list(model.parameters())), n_p, n_g))
+    print("Model Summary: %g layers, %g parameters, %g gradients" % (len(list(model.parameters())), n_p, n_g))
 
 
-def load_classifier(name='resnet101', n=2):
+def load_classifier(name="resnet101", n=2):
     # Loads a pretrained model reshaped to n-class output
 
     model = models.__dict__[name](pretrained=True)
-    # model = pretrainedmodels.__dict__[name](num_classes=1000, pretrained='imagenet')
+    # model = pretrainedmodels.__dict__[name](num_classes=1000, pretrained="imagenet")
 
     # Display model properties
     input_size = [3, 224, 224]
@@ -107,7 +106,7 @@ def load_classifier(name='resnet101', n=2):
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
     for x in [input_size, input_space, input_range, mean, std]:
-        print(x + ' =', eval(x))
+        print(x + " =", eval(x))
 
     # Reshape output to n classes
     filters = model.fc.weight.shape[1]
