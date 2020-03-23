98bc980297e20af3a55ffc389cf90e348a98dac6
Liu Changyu <liuchangyu1111@gmail.com>
2020-03-23 12:49:34 +0800

    Keep indent 4, and each line is less than 80

--- detect.py
+++ detect.py
@@ -37,26 +37,33 @@ from utils import select_device
 from utils import time_synchronized
 
 
-def detect(save_img=False):
+def detect(save_image=False):
     # (320, 192) or (416, 256) or (608, 352) for (height, width)
     image_size = (608, 352) if ONNX_EXPORT else args.image_size
-    out, source, weights, view_image, save_txt = args.output, args.source, args.weights, args.view_image, args.save_txt
-    camera = source == "0" or source.startswith("http") or source.endswith(".txt")
+    output = args.output
+    source = args.source
+    weight = args.weight
+    view_image = args.view_image
+    save_txt = args.save_txt
+
+    camera = False
+    if source == "0" or source.startswith("http") or source.endswith(".txt"):
+        camera = True
 
     # Initialize
     device = select_device(device="cpu" if ONNX_EXPORT else args.device)
-    if os.path.exists(out):
-        shutil.rmtree(out)  # delete output folder
-    os.makedirs(out)  # make new output folder
+    if os.path.exists(output):
+        shutil.rmtree(output)  # delete output folder
+    os.makedirs(output)  # make new output folder
 
     # Initialize model
     model = Darknet(args.cfg, image_size)
 
-    # Load weights
-    if weights.endswith(".pth"):
-        model.load_state_dict(torch.load(weights, map_location=device)["model"])
+    # Load weight
+    if weight.endswith(".pth"):
+        model.load_state_dict(torch.load(weight, map_location=device)["model"])
     else:
-        load_darknet_weights(model, weights)
+        load_darknet_weights(model, weight)
 
     # Second-stage classifier
     classify = False
@@ -64,7 +71,8 @@ def detect(save_img=False):
         # init model
         model_classifier = load_classifier(name="resnet101", n=2)
         # load model
-        model_classifier.load_state_dict(torch.load("weights/resnet101.pth", map_location=device)["model"])
+        model_classifier.load_state_dict(
+            torch.load("weights/resnet101.pth", map_location=device)["model"])
         model_classifier.to(device)
         model_classifier.eval()
     else:
@@ -79,29 +87,36 @@ def detect(save_img=False):
     if ONNX_EXPORT:
         model.fuse()
         image = torch.zeros((1, 3) + image_size)  # (1, 3, 608, 352)
-        filename = args.weights.replace(args.weights.split(".")[-1], "onnx")  # *.onnx filename
-        torch.onnx.export(model, tuple(image), filename, verbose=False, opset_version=11)
+        # *.onnx filename
+        filename = args.weights.replace(args.weights.split(".")[-1], "onnx")
+        torch.onnx.export(model,
+                          tuple(image),
+                          filename,
+                          verbose=False,
+                          opset_version=11)
 
         # Validate exported model
         import onnx
         model = onnx.load(filename)  # Load the ONNX model
         onnx.checker.check_model(model)  # Check that the IR is well formed
-        print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
+        # Print a human readable representation of the graph
+        print(onnx.helper.printable_graph(model.graph))
         return
 
     # Set Dataloader
     video_path, video_writer = None, None
     if camera:
         view_image = True
-        cudnn.benchmark = True  # set True to speed up constant image size inference
+        cudnn.benchmark = True
         dataset = LoadStreams(source, image_size=image_size)
     else:
-        save_img = True
+        save_image = True
         dataset = LoadImages(source, image_size=image_size)
 
     # Get names and colors
     names = load_classes(args.names)
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in
+              range(len(names))]
 
     # Run inference
     start_time = time.time()
@@ -118,7 +133,8 @@ def detect(save_img=False):
         t2 = time_synchronized()
 
         # Apply NMS
-        predict = non_max_suppression(predict, args.confidence_threshold, args.iou_threshold, classes=args.classes,
+        predict = non_max_suppression(predict, args.confidence_threshold,
+                                      args.iou_threshold, classes=args.classes,
                                       agnostic=args.agnostic_nms)
 
         # Apply Classifier
@@ -132,26 +148,32 @@ def detect(save_img=False):
             else:
                 p, context, im0 = image_path, "", im0s
 
-            save_path = str(Path(out) / Path(p).name)
+            save_path = str(Path(output) / Path(p).name)
             context += f"{image.shape[2]}*{image.shape[3]} "  # get image size
             if detect is not None and len(detect):
                 # Rescale boxes from img_size to im0 size
-                detect[:, :4] = scale_coords(image.shape[2:], detect[:, :4], im0.shape).round()
+                detect[:, :4] = scale_coords(image.shape[2:], detect[:, :4],
+                                             im0.shape).round()
 
                 # Print results
                 for classes in detect[:, -1].unique():
-                    number = (detect[:, -1] == classes).sum()  # detections per class
+                    # detections per class
+                    number = (detect[:, -1] == classes).sum()
                     context += f"{number} {names[int(classes)]}s, "
 
                 # Write results
                 for *xyxy, confidence, classes in detect:
                     if save_txt:  # Write to file
                         with open(save_path + ".txt", "a") as files:
-                            files.write(("%e " * 6 + "\n") % (*xyxy, classes, confidence))
+                            files.write(("%e " * 6 + "\n") % (
+                                *xyxy, classes, confidence))
 
-                    if save_img or view_image:  # Add bbox to image
+                    if save_image or view_image:  # Add bbox to image
                         label = f"{names[int(classes)]} {confidence * 100:.2f}%"
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(classes)])
+                        plot_one_box(xyxy,
+                                     im0,
+                                     label=label,
+                                     color=colors[int(classes)])
 
             # Stream results
             if view_image:
@@ -163,7 +185,7 @@ def detect(save_img=False):
             print(f"{context}Done. {t2 - t1:.3f}s")
 
             # Save results (image with detections)
-            if save_img:
+            if save_image:
                 if dataset.mode == "images":
                     cv2.imwrite(save_path, im0)
                 else:
@@ -175,7 +197,10 @@ def detect(save_img=False):
                         fps = video_capture.get(cv2.CAP_PROP_FPS)
                         w = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
                         h = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        video_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*args.fourcc), fps, (w, h))
+                        video_writer = cv2.VideoWriter(save_path,
+                                                       cv2.VideoWriter_fourcc(
+                                                           *args.fourcc), fps,
+                                                       (w, h))
                     video_writer.write(im0)
 
     print(f"Done. ({time.time() - start_time:.3f}s)")
@@ -183,11 +208,11 @@ def detect(save_img=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cfgs", type=str, default="cfgs/yolov3.cfgs",
-                        help="Neural network profile path. (default=cfgs/yolov3.cfgs)")
+    parser.add_argument("--cfgs", type=str, default="cfgs/yolov3.cfg",
+                        help="Neural network profile path. (default=cfgs/yolov3.cfg)")
     parser.add_argument("--names", type=str, default="data/coco.names",
                         help="Types of objects detected. (default=data/coco.names)")
-    parser.add_argument("--weights", type=str, default="weights/yolov3.pth",
+    parser.add_argument("--weight", type=str, default="weights/yolov3.pth",
                         help="Model file weight path. (default=weights/yolov3.pth")
     parser.add_argument("--source", type=str, default="data/examples",
                         help="Image input source. (default=data/examples)")
@@ -203,10 +228,14 @@ if __name__ == "__main__":
                         help="output video codec (verify ffmpeg support). (default=mp4v)")
     parser.add_argument("--device", default="",
                         help="device id (i.e. 0 or 0,1) or cpu. (default="")")
-    parser.add_argument("--view-image", action="store_true", help="Display results")
-    parser.add_argument("--save-txt", action="store_true", help="Save results to *.txt")
-    parser.add_argument("--classes", nargs="+", type=int, help="Filter by class")
-    parser.add_argument("--agnostic-nms", action="store_true", help="Class-agnostic NMS")
+    parser.add_argument("--view-image", action="store_true",
+                        help="Display results")
+    parser.add_argument("--save-txt", action="store_true",
+                        help="Save results to *.txt")
+    parser.add_argument("--classes", nargs="+", type=int,
+                        help="Filter by class")
+    parser.add_argument("--agnostic-nms", action="store_true",
+                        help="Class-agnostic NMS")
     args = parser.parse_args()
     print(args)
 
