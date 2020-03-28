a18ca0647da1ebb72dc36cc2b14f5f7e75170f64
Liu Changyu <liuchangyu1111@gmail.com>
2020-03-28 19:54:53 +0800

    Fix the cause of repeated function calls

--- easydet/data/common.py
+++ easydet/data/common.py
@@ -20,95 +20,12 @@ import shutil
 import cv2
 import numpy as np
 
-from .image import load_image
-
-
-def augment_hsv(image, hgain=0.5, sgain=0.5, vgain=0.5):
-    x = np.random.uniform(-1, 1, 3) * [hgain, sgain, vgain] + 1  # random gains
-    image_hsv = (cv2.cvtColor(image, cv2.COLOR_BGR2HSV) * x).clip(None, 255).astype(np.uint8)
-    np.clip(image_hsv[:, :, 0], None, 179, out=image_hsv[:, :, 0])  # inplace hue clip (0 - 179 deg)
-    cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR, dst=image)  # no return needed
-
 
 def create_folder(path="./output"):
     # Create folder
     if os.path.exists(path):
         shutil.rmtree(path)  # delete output folder
     os.makedirs(path)  # make new output folder
-
-
-def load_mosaic(self, index):
-    # loads images in a mosaic
-    labels4 = []
-    image_size = self.image_size
-    # mosaic center x, y
-    center_x, center_y = [int(random.uniform(image_size * 0.5, image_size * 1.5)) for _ in
-                          range(2)]
-    image4 = np.zeros((image_size * 2, image_size * 2, 3),
-                      dtype=np.uint8) + 128  # base image with 4 tiles
-    indices = [index] + [random.randint(0, len(self.labels) - 1) for _ in
-                         range(3)]  # 3 additional image indices
-    for i, index in enumerate(indices):
-        # Load image
-        image, _, (h, w) = load_image(self, index)
-
-        # place img in img4
-        if i == 0:  # top left
-            # xmin, ymin, xmax, ymax (large image)
-            x1a, y1a, x2a, y2a = max(center_x - w, 0), max(center_y - h, 0), center_x, center_y
-            x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (
-                    y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
-        elif i == 1:  # top right
-            x1a, y1a, x2a, y2a = center_x, max(center_y - h, 0), min(center_x + w,
-                                                                     image_size * 2), center_y
-            x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
-        elif i == 2:  # bottom left
-            x1a, y1a, x2a, y2a = max(center_x - w, 0), center_y, center_x, min(image_size * 2,
-                                                                               center_y + h)
-            x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(center_x, w), min(y2a - y1a, h)
-        elif i == 3:  # bottom right
-            x1a, y1a, x2a, y2a = center_x, center_y, min(center_x + w, image_size * 2), min(
-                image_size * 2,
-                center_y + h)
-            x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
-
-        image4[y1a:y2a, x1a:x2a] = image[y1b:y2b, x1b:x2b]  # image4[ymin:ymax, xmin:xmax]
-        padw = x1a - x1b
-        padh = y1a - y1b
-
-        # Load labels
-        label_path = self.label_files[index]
-        if os.path.isfile(label_path):
-            x = self.labels[index]
-            if x is None:  # labels not preloaded
-                with open(label_path, "r") as f:
-                    x = np.array([x.split() for x in f.read().splitlines()], dtype=np.float32)
-
-            if x.size > 0:
-                # Normalized xywh to pixel xyxy format
-                labels = x.copy()
-                labels[:, 1] = w * (x[:, 1] - x[:, 3] / 2) + padw
-                labels[:, 2] = h * (x[:, 2] - x[:, 4] / 2) + padh
-                labels[:, 3] = w * (x[:, 1] + x[:, 3] / 2) + padw
-                labels[:, 4] = h * (x[:, 2] + x[:, 4] / 2) + padh
-            else:
-                labels = np.zeros((0, 5), dtype=np.float32)
-            labels4.append(labels)
-
-    # Concat/clip labels
-    if len(labels4):
-        labels4 = np.concatenate(labels4, 0)
-        np.clip(labels4[:, 1:], 0, 2 * image_size, out=labels4[:, 1:])  # use with random_affine
-
-    # Augment
-    image4, labels4 = random_affine(image4, labels4,
-                                    degrees=self.hyp["degrees"] * 1,
-                                    translate=self.hyp["translate"] * 1,
-                                    scale=self.hyp["scale"] * 1,
-                                    shear=self.hyp["shear"] * 1,
-                                    border=-image_size // 2)  # border to remove
-
-    return image4, labels4
 
 
 def random_affine(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10, border=0):
