6a641a869d62e62921d0e2c669f43f399b3d18c2
Liu Changyu <liuchangyu1111@gmail.com>
2020-03-31 20:00:48 +0800

    Fix keyword errors

--- easydet/model/fuse.py
+++ easydet/model/fuse.py
@@ -63,20 +63,18 @@ class WeightFeatureFusion(nn.Module):
             x = x * w[0]
 
         # Fusion
-        nc = x.shape[1]  # input channels
+        nx = x.shape[1]  # input channels
         for i in range(self.num_layers - 1):
             # feature to add
-            a = outputs[self.layers[i]] * w[i + 1] if self.weight else outputs[
-                self.layers[i]]
-            ac = a.shape[1]  # feature channels
-            dc = nc - ac  # delta channels
+            a = outputs[self.layers[i]] * w[i + 1] if self.weight else outputs[self.layers[i]]
+            na = a.shape[1]  # feature channels
 
             # Adjust channels
-            if dc > 0:  # slice input
-                # or a = nn.ZeroPad2d((0, 0, 0, 0, 0, dc))(a); x = x + a
-                x[:, :ac] = x[:, :ac] + a
-            elif dc < 0:  # slice feature
-                x = x + a[:, :nc]
-            else:  # same shape
+            if nx == na:  # same shape
                 x = x + a
+            elif nx > na:  # slice input
+                x[:, :na] = x[:, :na] + a  # or a = nn.ZeroPad2d((0, 0, 0, 0, 0, dc))(a); x = x + a
+            else:  # slice feature
+                x = x + a[:, :nx]
+
         return x
