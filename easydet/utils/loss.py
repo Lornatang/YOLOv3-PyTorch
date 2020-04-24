def6cc0ca0dbcffe588e8cf21373249000ef0d6a
liuchangyu <liuchangyu1111@gmail.com>
2020-04-24 12:01:47 +0800

    Fixed errors in calculating loss

--- easydet/utils/loss.py
+++ easydet/utils/loss.py
@@ -18,49 +18,49 @@ from .iou import bbox_iou
 from .iou import wh_iou
 
 
-def build_targets(model, targets):
+def build_targets(pred, targets, model):
     # targets = [image, class, x, y, w, h]
-
-    nt = targets.shape[0]
+    targets_num = targets.shape[0]
     tcls, tbox, indices, anchor_vector = [], [], [], []
-    multi_gpu = type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
     reject, use_all_anchors = True, True
-    for i in model.yolo_layers:
+    gain = torch.ones(6, device=targets.device)  # normalized to gridspace gain
+
+    multi_gpu = type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
+    for i, j in enumerate(model.yolo_layers):
         # get number of grid points and anchor vec for this yolo layer
-        if multi_gpu:
-            ng, anchor_vec = model.module.module_list[i].ng, model.module.module_list[i].anchor_vec
-        else:
-            ng, anchor_vec = model.module_list[i].ng, model.module_list[i].anchor_vec
+        anchors = model.module.module_list[j].anchor_vector if multi_gpu else model.module_list[j].anchor_vector
 
         # iou of targets-anchors
-        t, a = targets, []
-        gwh = t[:, 4:6] * ng
-        if nt:
-            iou = wh_iou(anchor_vec, gwh)  # iou(3,n) = wh_iou(anchor_vec(3,2), gwh(n,2))
+        gain[2:] = torch.tensor(pred[i].shape)[[3, 2, 3, 2]]  # xyxy gain
+        t, a = targets * gain, []
+        gwh = t[:, 4:6]
+        if targets_num:
+            iou = wh_iou(anchors, gwh)  # iou(3,n) = wh_iou(anchor_vec(3,2), gwh(n,2))
 
             if use_all_anchors:
-                na = anchor_vec.shape[0]  # number of anchors
-                a = torch.arange(na).view((-1, 1)).repeat([1, nt]).view(-1)
+                na = anchors.shape[0]  # number of anchors
+                a = torch.arange(na).view((-1, 1)).repeat([1, targets_num]).view(-1)
                 t = targets.repeat([na, 1])
-                gwh = gwh.repeat([na, 1])
             else:  # use best anchor only
                 iou, a = iou.max(0)  # best iou and anchor
 
             # reject anchors below iou_thres (OPTIONAL, increases P, lowers R)
             if reject:
-                j = iou.view(-1) > model.hyp["iou_t"]  # iou threshold hyperparameter
-                t, a, gwh = t[j], a[j], gwh[j]
+                print(model.hyper_parameters["iou_t"])
+                j = iou.view(-1) > model.hyper_parameters["iou_t"]  # iou threshold hyperparameter
+                t, a = t[j], a[j]
 
         # Indices
         b, c = t[:, :2].long().t()  # target image, class
-        gxy = t[:, 2:4] * ng  # grid x, y
+        gxy = t[:, 2:4]  # grid x, y
+        gwh = t[:, 4:6]  # grid w, h
         gi, gj = gxy.long().t()  # grid x, y indices
         indices.append((b, a, gj, gi))
 
         # Box
         gxy -= gxy.floor()  # xy
         tbox.append(torch.cat((gxy, gwh), 1))  # xywh (grids)
-        anchor_vector.append(anchor_vec[a])
+        anchor_vector.append(anchors[a])
 
         # Class
         tcls.append(c)
@@ -70,29 +70,29 @@ def build_targets(model, targets):
     return tcls, tbox, indices, anchor_vector
 
 
-def compute_loss(p, targets, model):  # predictions, targets, model
-    ft = torch.cuda.FloatTensor if p[0].is_cuda else torch.Tensor
+def compute_loss(pred, targets, model):  # predictions, targets, model
+    ft = torch.cuda.FloatTensor if pred[0].is_cuda else torch.Tensor
     lcls, lbox, lobj = ft([0]), ft([0]), ft([0])
-    tcls, tbox, indices, anchor_vec = build_targets(model, targets)
-    h = model.hyp  # hyperparameters
-    red = "mean"  # Loss reduction (sum or mean)
+    tcls, tbox, indices, anchor_vector = build_targets(pred, targets, model)
+    hyper_parameters = model.hyper_parameters  # hyperparameters
+    reduction = "mean"  # Loss reduction (sum or mean)
 
     # Define criteria
-    BCEcls = nn.BCEWithLogitsLoss(pos_weight=ft([h["cls_pw"]]), reduction=red)
-    BCEobj = nn.BCEWithLogitsLoss(pos_weight=ft([h["obj_pw"]]), reduction=red)
+    BCEcls = nn.BCEWithLogitsLoss(pos_weight=ft([hyper_parameters["cls_pw"]]), reduction=reduction)
+    BCEobj = nn.BCEWithLogitsLoss(pos_weight=ft([hyper_parameters["obj_pw"]]), reduction=reduction)
 
     # class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
     cp, cn = smooth_BCE(eps=0.0)
 
     # focal loss
-    g = h["fl_gamma"]  # focal loss gamma
+    g = hyper_parameters["fl_gamma"]  # focal loss gamma
     if g > 0:
         BCEcls, BCEobj = FocalLoss(BCEcls, g), FocalLoss(BCEobj, g)
 
     # Compute losses
     np, ng = 0, 0  # number grid points, targets
     tobj = 0.
-    for i, pi in enumerate(p):  # layer index, layer predictions
+    for i, pi in enumerate(pred):  # layer index, layer predictions
         b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
         tobj = torch.zeros_like(pi[..., 0])  # target obj
         np += tobj.numel()
@@ -106,12 +106,11 @@ def compute_loss(p, targets, model):  # predictions, targets, model
 
             # GIoU
             pxy = torch.sigmoid(ps[:, 0:2])  # pxy = pxy * s - (s - 1) / 2,  s = 1.5  (scale_xy)
-            pwh = torch.exp(ps[:, 2:4]).clamp(max=1E3) * anchor_vec[i]
+            pwh = torch.exp(ps[:, 2:4]).clamp(max=1E3) * anchor_vector[i]
             pbox = torch.cat((pxy, pwh), 1)  # predicted box
             giou = bbox_iou(pbox.t(), tbox[i], x1y1x2y2=False, GIoU=True)  # giou computation
-            lbox += (1.0 - giou).sum() if red == "sum" else (1.0 - giou).mean()  # giou loss
-            tobj[b, a, gj, gi] = (1.0 - model.gr) + model.gr * giou.detach().clamp(0).type(
-                tobj.dtype)  # giou ratio
+            lbox += (1.0 - giou).sum() if reduction == "sum" else (1.0 - giou).mean()  # giou loss
+            tobj[b, a, gj, gi] = (1.0 - model.gr) + model.gr * giou.detach().clamp(0).type(tobj.dtype)  # giou ratio
 
             if model.nc > 1:  # cls loss (only if multiple classes)
                 t = torch.full_like(ps[:, 5:], cn)  # targets
@@ -120,12 +119,12 @@ def compute_loss(p, targets, model):  # predictions, targets, model
 
         lobj += BCEobj(pi[..., 4], tobj)  # obj loss
 
-    lbox *= h["giou"]
-    lobj *= h["obj"]
-    lcls *= h["cls"]
-    if red == "sum":
-        bs = tobj.shape[0]  # batch size
-        lobj *= 3 / (6300 * bs) * 2  # 3 / np * 2
+    lbox *= hyper_parameters["giou"]
+    lobj *= hyper_parameters["obj"]
+    lcls *= hyper_parameters["cls"]
+    if reduction == "sum":
+        batch_size = tobj.shape[0]  # batch size
+        lobj *= 3 / (6300 * batch_size) * 2  # 3 / np * 2
         if ng:
             lcls *= 3 / ng / model.nc
             lbox *= 3 / ng
